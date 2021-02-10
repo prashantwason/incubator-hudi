prashantwason/incubator-hudi

commit 4fb2803aed7e71a909c6cbeed179d7217ac718f6

[UBER] Downgrade Metrics 4.1.1 -> 3.1.5
Summary: Copying over D4758225

Reviewers: O955 Project Hoodie Project Reviewer: Add blocking reviewers!, PHID-PROJ-pxfpotkfgkanblb3detq!, #ldap_hudi, pwason

Reviewed By: #ldap_hudi, pwason

Differential Revision: https://code.uberinternal.com/D5415269

hudi-client/hudi-client-common/pom.xml
@@ -75,10 +75,6 @@
       <artifactId>metrics-core</artifactId>
     </dependency>
     <dependency>
-      <groupId>io.dropwizard.metrics</groupId>
-      <artifactId>metrics-jmx</artifactId>
-    </dependency>
-    <dependency>
       <groupId>io.prometheus</groupId>
       <artifactId>simpleclient</artifactId>
     </dependency>

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metrics/JmxReporterServer.java
@@ -23,7 +23,7 @@ import org.apache.hudi.common.util.ValidationUtils;
 import org.apache.hudi.exception.HoodieException;
 
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.jmx.JmxReporter;
+import com.codahale.metrics.JmxReporter;
 
 import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metrics/Metrics.java
@@ -23,6 +23,7 @@ import org.apache.hudi.common.util.Option;
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieException;
 
+import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -125,8 +126,9 @@ public class Metrics {
   public static void registerGauge(String metricName, final long value) {
     try {
       MetricRegistry registry = Metrics.getInstance().getRegistry();
-      HoodieGauge guage = (HoodieGauge) registry.gauge(metricName, () -> new HoodieGauge<>(value));
-      guage.setValue(value);
+      Counter counter = new Counter();
+      counter.inc(value);
+      registry.register(metricName, counter);
     } catch (Exception e) {
       // Here we catch all exception, so the major upsert pipeline will not be affected if the
       // metrics system has some issues.

pom.xml
@@ -102,7 +102,7 @@
     <hive.groupid>org.apache.hive</hive.groupid>
     <hive.version>2.3.1</hive.version>
     <hive.exec.classifier>core</hive.exec.classifier>
-    <metrics.version>4.1.1</metrics.version>
+    <metrics.version>3.1.5</metrics.version>
     <prometheus.version>0.8.0</prometheus.version>
     <http.version>4.4.1</http.version>
     <spark.version>2.4.4</spark.version>
@@ -539,11 +539,7 @@
         <artifactId>metrics-core</artifactId>
         <version>${metrics.version}</version>
       </dependency>
-      <dependency>
-        <groupId>io.dropwizard.metrics</groupId>
-        <artifactId>metrics-jmx</artifactId>
-        <version>${metrics.version}</version>
-      </dependency>
+
 
       <dependency>
         <groupId>io.prometheus</groupId>

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieConsoleMetrics.java
@@ -47,6 +47,6 @@ public class TestHoodieConsoleMetrics {
   @Test
   public void testRegisterGauge() {
     registerGauge("metric1", 123L);
-    assertEquals("123", Metrics.getInstance().getRegistry().getGauges().get("metric1").getValue().toString());
+    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters().get("metric1").getCount());
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieJmxMetrics.java
@@ -43,8 +43,8 @@ public class TestHoodieJmxMetrics {
     when(config.getJmxPort()).thenReturn(String.valueOf(NetworkTestUtils.nextFreePort()));
     new HoodieMetrics(config, "raw_table");
     registerGauge("jmx_metric1", 123L);
-    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
-        .get("jmx_metric1").getValue().toString());
+    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
+        .get("jmx_metric1").getCount());
   }
 
   @Test
@@ -55,7 +55,7 @@ public class TestHoodieJmxMetrics {
     when(config.getJmxPort()).thenReturn(String.valueOf(NetworkTestUtils.nextFreePort()));
     new HoodieMetrics(config, "raw_table");
     registerGauge("jmx_metric2", 123L);
-    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
-        .get("jmx_metric2").getValue().toString());
+    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
+        .get("jmx_metric2").getCount());
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieMetrics.java
@@ -49,7 +49,7 @@ public class TestHoodieMetrics {
   @Test
   public void testRegisterGauge() {
     registerGauge("metric1", 123L);
-    assertEquals("123", Metrics.getInstance().getRegistry().getGauges().get("metric1").getValue().toString());
+    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters().get("metric1").getCount());
   }
 
   @Test
@@ -61,7 +61,7 @@ public class TestHoodieMetrics {
     Thread.sleep(5); // Ensure timer duration is > 0
     metrics.updateIndexMetrics("some_action", metrics.getDurationInMs(timer.stop()));
     String metricName = metrics.getMetricsName("index", "some_action.duration");
-    long msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
+    long msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
     assertTrue(msec > 0);
 
     // Rollback metrics
@@ -70,10 +70,10 @@ public class TestHoodieMetrics {
     long numFilesDeleted = 1 + rand.nextInt();
     metrics.updateRollbackMetrics(metrics.getDurationInMs(timer.stop()), numFilesDeleted);
     metricName = metrics.getMetricsName("rollback", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
+    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("rollback", "numFilesDeleted");
-    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesDeleted);
+    assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesDeleted);
 
     // Clean metrics
     timer = metrics.getRollbackCtx();
@@ -81,10 +81,10 @@ public class TestHoodieMetrics {
     numFilesDeleted = 1 + rand.nextInt();
     metrics.updateCleanMetrics(metrics.getDurationInMs(timer.stop()), (int)numFilesDeleted);
     metricName = metrics.getMetricsName("clean", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
+    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("clean", "numFilesDeleted");
-    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesDeleted);
+    assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesDeleted);
 
     // Finalize metrics
     timer = metrics.getFinalizeCtx();
@@ -92,10 +92,10 @@ public class TestHoodieMetrics {
     long numFilesFinalized = 1 + rand.nextInt();
     metrics.updateFinalizeWriteMetrics(metrics.getDurationInMs(timer.stop()), (int)numFilesFinalized);
     metricName = metrics.getMetricsName("finalize", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
+    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("finalize", "numFilesFinalized");
-    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesFinalized);
+    assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesFinalized);
 
     // Commit / deltacommit / compaction metrics
     Stream.of("commit", "deltacommit", "compaction").forEach(action -> {
@@ -126,36 +126,36 @@ public class TestHoodieMetrics {
       metrics.updateCommitMetrics(randomValue + 14, commitTimer.stop(), metadata, action);
 
       String metricname = metrics.getMetricsName(action, "duration");
-      long duration = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue();
+      long duration = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount();
       assertTrue(duration > 0);
       metricname = metrics.getMetricsName(action, "totalPartitionsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalPartitionsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalPartitionsWritten());
       metricname = metrics.getMetricsName(action, "totalFilesInsert");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalFilesInsert());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalFilesInsert());
       metricname = metrics.getMetricsName(action, "totalFilesUpdate");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalFilesUpdated());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalFilesUpdated());
       metricname = metrics.getMetricsName(action, "totalRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalUpdateRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalUpdateRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalUpdateRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalInsertRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalInsertRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalInsertRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalBytesWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalBytesWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalBytesWritten());
       metricname = metrics.getMetricsName(action, "commitTime");
-      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), randomValue + 14);
+      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), randomValue + 14);
       metricname = metrics.getMetricsName(action, "totalScanTime");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalScanTime());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalScanTime());
       metricname = metrics.getMetricsName(action, "totalCreateTime");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalCreateTime());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalCreateTime());
       metricname = metrics.getMetricsName(action, "totalUpsertTime");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalUpsertTime());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalUpsertTime());
       metricname = metrics.getMetricsName(action, "totalCompactedRecordsUpdated");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalCompactedRecordsUpdated());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalCompactedRecordsUpdated());
       metricname = metrics.getMetricsName(action, "totalLogFilesCompacted");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalLogFilesCompacted());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalLogFilesCompacted());
       metricname = metrics.getMetricsName(action, "totalLogFilesSize");
-      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalLogFilesSize());
+      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalLogFilesSize());
     });
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/prometheus/TestPushGateWayReporter.java
@@ -42,7 +42,7 @@ public class TestPushGateWayReporter {
     when(config.getPushGatewayReportPeriodSeconds()).thenReturn(10);
     new HoodieMetrics(config, "raw_table");
     registerGauge("pushGateWayReporter_metric", 123L);
-    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
-        .get("pushGateWayReporter_metric").getValue().toString());
+    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
+        .get("pushGateWayReporter_metric").getCount());
   }
 }

commit 4c311f65e8e0a54ad4a0aec51bda4413a8577584

[UBER] Reverting Downgrade Metrics 4.1.1 -> 3.1.5"
This reverts commit 4fb2803aed7e71a909c6cbeed179d7217ac718f6 which broke the graphana dashboards because Counters are published with a .count suffix to the metric name and our dashboards are not setup for that suffix.

hudi-client/hudi-client-common/pom.xml
@@ -75,6 +75,10 @@
       <artifactId>metrics-core</artifactId>
     </dependency>
     <dependency>
+      <groupId>io.dropwizard.metrics</groupId>
+      <artifactId>metrics-jmx</artifactId>
+    </dependency>
+    <dependency>
       <groupId>io.prometheus</groupId>
       <artifactId>simpleclient</artifactId>
     </dependency>

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metrics/JmxReporterServer.java
@@ -23,7 +23,7 @@ import org.apache.hudi.common.util.ValidationUtils;
 import org.apache.hudi.exception.HoodieException;
 
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.jmx.JmxReporter;
 
 import javax.management.MBeanServer;
 import javax.management.remote.JMXConnectorServer;

hudi-client/hudi-client-common/src/main/java/org/apache/hudi/metrics/Metrics.java
@@ -23,7 +23,6 @@ import org.apache.hudi.common.util.Option;
 import org.apache.hudi.config.HoodieWriteConfig;
 import org.apache.hudi.exception.HoodieException;
 
-import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -126,9 +125,8 @@ public class Metrics {
   public static void registerGauge(String metricName, final long value) {
     try {
       MetricRegistry registry = Metrics.getInstance().getRegistry();
-      Counter counter = new Counter();
-      counter.inc(value);
-      registry.register(metricName, counter);
+      HoodieGauge guage = (HoodieGauge) registry.gauge(metricName, () -> new HoodieGauge<>(value));
+      guage.setValue(value);
     } catch (Exception e) {
       // Here we catch all exception, so the major upsert pipeline will not be affected if the
       // metrics system has some issues.

pom.xml
@@ -102,7 +102,7 @@
     <hive.groupid>org.apache.hive</hive.groupid>
     <hive.version>2.3.1</hive.version>
     <hive.exec.classifier>core</hive.exec.classifier>
-    <metrics.version>3.1.5</metrics.version>
+    <metrics.version>4.1.1</metrics.version>
     <prometheus.version>0.8.0</prometheus.version>
     <http.version>4.4.1</http.version>
     <spark.version>2.4.4</spark.version>
@@ -539,7 +539,11 @@
         <artifactId>metrics-core</artifactId>
         <version>${metrics.version}</version>
       </dependency>
-
+      <dependency>
+        <groupId>io.dropwizard.metrics</groupId>
+        <artifactId>metrics-jmx</artifactId>
+        <version>${metrics.version}</version>
+      </dependency>
 
       <dependency>
         <groupId>io.prometheus</groupId>

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieConsoleMetrics.java
@@ -47,6 +47,6 @@ public class TestHoodieConsoleMetrics {
   @Test
   public void testRegisterGauge() {
     registerGauge("metric1", 123L);
-    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters().get("metric1").getCount());
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges().get("metric1").getValue().toString());
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieJmxMetrics.java
@@ -43,8 +43,8 @@ public class TestHoodieJmxMetrics {
     when(config.getJmxPort()).thenReturn(String.valueOf(NetworkTestUtils.nextFreePort()));
     new HoodieMetrics(config, "raw_table");
     registerGauge("jmx_metric1", 123L);
-    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
-        .get("jmx_metric1").getCount());
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
+        .get("jmx_metric1").getValue().toString());
   }
 
   @Test
@@ -55,7 +55,7 @@ public class TestHoodieJmxMetrics {
     when(config.getJmxPort()).thenReturn(String.valueOf(NetworkTestUtils.nextFreePort()));
     new HoodieMetrics(config, "raw_table");
     registerGauge("jmx_metric2", 123L);
-    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
-        .get("jmx_metric2").getCount());
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
+        .get("jmx_metric2").getValue().toString());
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/TestHoodieMetrics.java
@@ -49,7 +49,7 @@ public class TestHoodieMetrics {
   @Test
   public void testRegisterGauge() {
     registerGauge("metric1", 123L);
-    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters().get("metric1").getCount());
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges().get("metric1").getValue().toString());
   }
 
   @Test
@@ -61,7 +61,7 @@ public class TestHoodieMetrics {
     Thread.sleep(5); // Ensure timer duration is > 0
     metrics.updateIndexMetrics("some_action", metrics.getDurationInMs(timer.stop()));
     String metricName = metrics.getMetricsName("index", "some_action.duration");
-    long msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
+    long msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
     assertTrue(msec > 0);
 
     // Rollback metrics
@@ -70,10 +70,10 @@ public class TestHoodieMetrics {
     long numFilesDeleted = 1 + rand.nextInt();
     metrics.updateRollbackMetrics(metrics.getDurationInMs(timer.stop()), numFilesDeleted);
     metricName = metrics.getMetricsName("rollback", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
+    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("rollback", "numFilesDeleted");
-    assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesDeleted);
+    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesDeleted);
 
     // Clean metrics
     timer = metrics.getRollbackCtx();
@@ -81,10 +81,10 @@ public class TestHoodieMetrics {
     numFilesDeleted = 1 + rand.nextInt();
     metrics.updateCleanMetrics(metrics.getDurationInMs(timer.stop()), (int)numFilesDeleted);
     metricName = metrics.getMetricsName("clean", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
+    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("clean", "numFilesDeleted");
-    assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesDeleted);
+    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesDeleted);
 
     // Finalize metrics
     timer = metrics.getFinalizeCtx();
@@ -92,10 +92,10 @@ public class TestHoodieMetrics {
     long numFilesFinalized = 1 + rand.nextInt();
     metrics.updateFinalizeWriteMetrics(metrics.getDurationInMs(timer.stop()), (int)numFilesFinalized);
     metricName = metrics.getMetricsName("finalize", "duration");
-    msec = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount();
+    msec = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue();
     assertTrue(msec > 0);
     metricName = metrics.getMetricsName("finalize", "numFilesFinalized");
-    assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricName).getCount(), numFilesFinalized);
+    assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricName).getValue(), numFilesFinalized);
 
     // Commit / deltacommit / compaction metrics
     Stream.of("commit", "deltacommit", "compaction").forEach(action -> {
@@ -126,36 +126,36 @@ public class TestHoodieMetrics {
       metrics.updateCommitMetrics(randomValue + 14, commitTimer.stop(), metadata, action);
 
       String metricname = metrics.getMetricsName(action, "duration");
-      long duration = (Long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount();
+      long duration = (Long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue();
       assertTrue(duration > 0);
       metricname = metrics.getMetricsName(action, "totalPartitionsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalPartitionsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalPartitionsWritten());
       metricname = metrics.getMetricsName(action, "totalFilesInsert");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalFilesInsert());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalFilesInsert());
       metricname = metrics.getMetricsName(action, "totalFilesUpdate");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalFilesUpdated());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalFilesUpdated());
       metricname = metrics.getMetricsName(action, "totalRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalUpdateRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalUpdateRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalUpdateRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalInsertRecordsWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalInsertRecordsWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalInsertRecordsWritten());
       metricname = metrics.getMetricsName(action, "totalBytesWritten");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.fetchTotalBytesWritten());
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.fetchTotalBytesWritten());
       metricname = metrics.getMetricsName(action, "commitTime");
-      assertEquals((long)Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), randomValue + 14);
+      assertEquals((long)Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), randomValue + 14);
       metricname = metrics.getMetricsName(action, "totalScanTime");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalScanTime());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalScanTime());
       metricname = metrics.getMetricsName(action, "totalCreateTime");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalCreateTime());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalCreateTime());
       metricname = metrics.getMetricsName(action, "totalUpsertTime");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalUpsertTime());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalUpsertTime());
       metricname = metrics.getMetricsName(action, "totalCompactedRecordsUpdated");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalCompactedRecordsUpdated());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalCompactedRecordsUpdated());
       metricname = metrics.getMetricsName(action, "totalLogFilesCompacted");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalLogFilesCompacted());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalLogFilesCompacted());
       metricname = metrics.getMetricsName(action, "totalLogFilesSize");
-      assertEquals(Metrics.getInstance().getRegistry().getCounters().get(metricname).getCount(), metadata.getTotalLogFilesSize());
+      assertEquals(Metrics.getInstance().getRegistry().getGauges().get(metricname).getValue(), metadata.getTotalLogFilesSize());
     });
   }
 }

hudi-client/hudi-client-common/src/test/java/org/apache/hudi/metrics/prometheus/TestPushGateWayReporter.java
@@ -42,7 +42,7 @@ public class TestPushGateWayReporter {
     when(config.getPushGatewayReportPeriodSeconds()).thenReturn(10);
     new HoodieMetrics(config, "raw_table");
     registerGauge("pushGateWayReporter_metric", 123L);
-    assertEquals(123L, Metrics.getInstance().getRegistry().getCounters()
-        .get("pushGateWayReporter_metric").getCount());
+    assertEquals("123", Metrics.getInstance().getRegistry().getGauges()
+        .get("pushGateWayReporter_metric").getValue().toString());
   }
 }